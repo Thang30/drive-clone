d8b0fbe8ded2002e1c130a9261176a3fb593720c
Thang30 <fsnthang30@gmail.com>
2025-04-13 06:31:45 +0300

    style: change the style of Upload button (#2)
    * style: change the style of Upload button
    
    * style: add favicon and chnage webapp title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,18 @@
-import { Upload } from "lucide-react"
-import { Button } from "~/components/ui/button"
+import { Upload } from "lucide-react";
+import { Button } from "~/components/ui/button";
 
 export function Header() {
   return (
     <header className="bg-gray-800 py-4">
-      <div className="container mx-auto px-4 flex justify-between items-center">
+      <div className="container mx-auto flex items-center justify-between px-4">
         <h1 className="text-2xl font-bold">Drive Clone</h1>
-        <Button variant="outline">
+        <Button
+          variant="default"
+          className="hover:bg-secondary hover:text-black"
+        >
           <Upload className="mr-2 h-4 w-4" /> Upload
         </Button>
       </div>
     </header>
-  )
+  );
 }
-
