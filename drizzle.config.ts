ea09886eaf6365dff2e9e4f9995acaf53aa018f3
Thang30 <fsnthang30@gmail.com>
2025-04-13 06:44:50 +0300

    chore: change tablesFilter again

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ import { env } from "~/env";
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "singlestore",
-  tablesFilter: ["db_f0b72_*"],
+  tablesFilter: ["starter-workspace_*"],
   dbCredentials: {
     host: env.SINGLESTORE_HOST,
     port: parseInt(env.SINGLESTORE_PORT),
